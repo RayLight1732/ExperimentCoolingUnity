660164cce4bb4da2717b916aab3898fddeee131a
RayLight1732 <56024986+RayLight1732@users.noreply.github.com>
2025-07-14 17:59:52 +0900

    update

--- Assets/My/Scripts/GameManager.cs
+++ Assets/My/Scripts/GameManager.cs
@@ -82,6 +82,7 @@ public class GameManager : SendMessageEventProvider
         }
         float rot_y = main_camera.transform.localEulerAngles.y;
         camera_offset.transform.Rotate(0,-1*rot_y,0);
+        camera_offset.transform.position -= main_camera.transform.localPosition;
         Debug.Log("Start game");
     }
 
